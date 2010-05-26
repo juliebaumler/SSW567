77c6eed350213510c5f07f77d02682bab22bed0e
Lisa Jankovich <lisajank@sbcglobal.net>
2010-05-26 08:13:48 +0800

    25 May 2010 Update to file to add output dialog box.

--- Triangles/Main.java
+++ Triangles/Main.java
@@ -8,7 +8,8 @@ package sw_test567_assignment1;
 // Assignment 1 - Triangle Program
 import javax.swing.JOptionPane;
 
-public class Main {
+
+public class MainA {
   /**Main method*/
   public static void main(String[] args) {
 
@@ -36,37 +37,56 @@ public class Main {
       "Assignment 1 Input", JOptionPane.QUESTION_MESSAGE);
     // Convert string to double
     double side_z = Double.parseDouble(numberString);
+    
+    String strTriOut = "";
+    String equil = "";
+    String isos = "";
+    String scal = "";
 
+    {
     //Triangle Determination
 
     //Right Triangle
     if (((side_x * side_x) + (side_y * side_y)) == (side_z * side_z))
-    System.out.println("right triangle");
+    {   System.out.println("right triangle");
+        strTriOut = "right triangle and ";}
+    
     if (((side_x * side_x) + (side_z * side_z)) == (side_y * side_y))
-    System.out.println("right triangle");
+    {   System.out.println("right triangle");
+        strTriOut = "right triangle and ";}
+        
     if (((side_z * side_z) + (side_y * side_y)) == (side_x * side_x))
-    System.out.println("right triangle");
-
-
+    {   System.out.println("right triangle");
+        strTriOut = "right triangle and ";}
+        
+   
     //equilateral triangle
     if ((side_x == side_y) && (side_x == side_z) && (side_y == side_z))
-    System.out.println("equilateral triangle");
+    
+    {System.out.println("equilateral triangle");
+    equil += "equilateral triangle "; }
 
     //isosceles triangle
     if (((side_x == side_y) || (side_y == side_z) || (side_x == side_z)) && !((side_x == side_y) && (side_x == side_z) && (side_y == side_z)))
-    System.out.println("isosceles triangle");
+    {System.out.println("isosceles triangle");
+    isos += "isosceles triangle "; }
 
     //scalene triangle
     else if (!((side_x == side_y) && (side_x == side_z) && (side_y == side_z)))
-    System.out.println("scalene triangle");
+    {System.out.println("scalene triangle");
+    scal += "scalene triangle "; }
 
-    //Display the resultant triangle outputs
-//  "Assignment 1 Input" = JOptionPane.showoutputDialog(null,
-//      "The triangle is ",
-//      "Main Input", JOptionPane.QUESTION_MESSAGE);
+// Dialog Box Output for Showing Triangle Type
    
+ JOptionPane.showMessageDialog(null,
+      "The trinagle type is: " + strTriOut + equil + isos + scal,
+      "Assignment 1 Output", JOptionPane.INFORMATION_MESSAGE);
+
 
     System.exit(0);
   }
 }
 
+}
+
+// NOTES: wiki link for test data on right triangles: http://en.wikipedia.org/wiki/Pythagorean_theorem
