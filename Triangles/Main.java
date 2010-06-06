77c4ee162d180c11202b1d43b8b97977885316dc
Julie Baumler <julie@metaltoad.com>
2010-06-06 16:55:12 -0700

    Improved error handling (Fixes for bug id's 3-8) and typo in last commit

--- Triangles/Main.java
+++ Triangles/Main.java
@@ -40,15 +40,22 @@ public class Main {
     	side_x = Double.parseDouble(numberString1);
       side_y = Double.parseDouble(numberString2);
     	side_z = Double.parseDouble(numberString3);
-				System.out.println(side_x + " " + side_y + " " + side_z );
+			//System.out.println(side_x + " " + side_y + " " + side_z );
 		}
 		catch ( NumberFormatException ex ) {
 			JOptionPane.showMessageDialog( null, 
 				"Entries for all sides must be numbers, ie (1.12347, 3, 1.234e2)", 
-				"Error:", JOptionPane.INFORMATION_MESSAGE);
+				"Error:", JOptionPane.ERROR_MESSAGE);
     	System.exit(1);
 			
 		}
+		catch ( NullPointerException ex ) {
+			JOptionPane.showMessageDialog( null, 
+				"Values for all three sides are required.", 
+				"Error:", JOptionPane.ERROR_MESSAGE);
+    	System.exit(1);
+		}
+
 
 		// assign the sides to a sorted array
 		// this means sides[0] is the shortest side
@@ -61,56 +68,69 @@ public class Main {
 			check_valid_triangle( sides );
 		
     	String strTriOut = "";
-    	String equil = "";
-    	String isos = "";
-    	String scal = "";
 
     	//Triangle Determination
-
-    	//Right Triangle
-    	if ( ( Math.pow(sides[0], 2) + Math.pow(sides[1], 2)) == Math.pow(sides[2], 2) )
-    	{
-				System.out.println("right triangle");
-        strTriOut = "right triangle and ";
-			}
-    
     	//equilateral triangle
     	if ( sides[0] == sides[2] )
     	{
 				System.out.println("equilateral triangle");
-    		equil += "equilateral triangle "; 
+    		strTriOut = " equilateral triangle "; 
 			}
-    	//isosceles triangle
-    	else if ( sides[0] == sides[1] )
-    	{
-				System.out.println("isosceles triangle");
-    		isos += "isosceles triangle "; 
-			}
-    	//scalene triangle
-    	else 
-    	{
-				System.out.println("scalene triangle");
-    		scal += "scalene triangle "; 
+			else
+			{
+
+    		//Right Triangle
+    		if ( ( Math.pow(sides[0], 2) + Math.pow(sides[1], 2)) == Math.pow(sides[2], 2) )
+    		{
+					System.out.println("right triangle");
+						if ( Double.isInfinite( Math.pow(sides[2], 2) ) )
+						{
+       	 			strTriOut = "Due to size of triangle, unable to determine whether it is a right triangle. ";
+						}
+						else
+						{
+       	 			strTriOut = " right triangle and ";
+						}
+				} 
+
+    		//isosceles triangle
+    		if ( sides[0] == sides[1] )
+    		{
+					System.out.println("isosceles triangle");
+    			strTriOut += " isosceles triangle "; 
+				}
+    		//scalene triangle
+    		else 
+    		{
+					System.out.println("scalene triangle");
+    			strTriOut += " scalene triangle "; 
+				}
 			}
 
 			// Dialog Box Output for Showing Triangle Type
  			JOptionPane.showMessageDialog(null,
-      	"The triangle type is: " + strTriOut + equil + isos + scal,
+      	"The triangle type is:" + strTriOut, 
       	"Assignment 1 Output", JOptionPane.INFORMATION_MESSAGE);
     	System.exit(0);
 		}
 		catch ( TriangleException triex )
   	{
 			JOptionPane.showMessageDialog( null, 
-				triex.getMessage(), "Error:", JOptionPane.INFORMATION_MESSAGE);
+				triex.getMessage(), "Error:", JOptionPane.ERROR_MESSAGE);
     	System.exit(1);
 		}
 	}
 
   protected static void check_valid_triangle( double triangle[] ) throws TriangleException {
+
+
 		if ( triangle[0] <= 0 )
 		{
-			throw new TriangleException("Triangles cannot be formed with sides of length less than zero.");
+			throw new TriangleException("Triangles cannot be formed with sides of length zero or less.");
+		}
+		else if ( Double.isInfinite( triangle[2] ) )
+		{
+			throw new TriangleException("Sorry cannot process triangles with sides longer than " + Double.MAX_VALUE);
 		}
 		else if ( (triangle[0] + triangle[1] ) < triangle[2] )
 		{
